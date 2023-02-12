8d3c3b35bb422011f7f2252bdd0997b350a90999
DmitryOb <79513646222@yandex.ru>
2023-02-12 14:57:49 +0300

    add OFFLINE_CONDITION_TIME

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import {getDatabase, ref} from "firebase/database";
 import app from "./firebase-web-app-config";
 import {useObjectVal} from "react-firebase-hooks/database";
 import PrivacyContent from "./PrivacyContent/PrivacyContent";
-import {memo} from "react";
-
+import {memo, useRef, useState} from "react";
+import {useInterval} from "react-use";
 const db = getDatabase(app);
 const randomIdQuery = ref(db, "randomId");
 const temperatureQuery = ref(db, "temperature");
@@ -13,19 +13,43 @@ const humidityQuery = ref(db, "humidity");
 
 const PrivacyContentMemo = memo(PrivacyContent);
 
+const OFFLINE_CONDITION_TIME = 3000;
+
 function App() {
-  const [randomId, loadingR, errorR] = useObjectVal<number, string, string>(randomIdQuery);
   const [temperature, loadingT, errorT] = useObjectVal<number, string, string>(temperatureQuery);
   const [humidity, loadingH, errorH] = useObjectVal<number, string, string>(humidityQuery);
 
+  const [randomId, loadingR, errorR] = useObjectVal<number, string, string>(randomIdQuery);
+  const lastRandomIdRef = useRef(randomId);
+  const [lastTimeUpID, setLastTimeUpID] = useState(new Date().valueOf());
+  const [isOnline, setIsOnline] = useState(false)
+
+  useInterval(
+    () => {
+      // if more than OFFLINE_CONDITION_TIME milliseconds randomId not changes it does it mean that we are offline
+      new Date().valueOf() - lastTimeUpID > OFFLINE_CONDITION_TIME ? setIsOnline(false) : setIsOnline(true);
+      if (lastRandomIdRef.current !== randomId) {
+        lastRandomIdRef.current = randomId;
+        setLastTimeUpID(new Date().valueOf())
+      }
+    },
+    500,
+  );
+
   return (
     <div className="App">
-      <h2>ESP8266. Информация о температуре в помещении:</h2>
-      <span>ID: {!errorR && !loadingR && randomId && randomId}</span>
-      <h3 style={{color: getColor(temperature ?? null)}}>
-        Температура: {!errorT && !loadingT && temperature && temperature}
-      </h3>
+      <h4>ESP8266 DHT11 влажность и температура</h4>
+
+      <div className={'status-row'}>
+        <span className={'status-text'}>{isOnline ? 'ONLINE' : 'OFFLINE'}</span>
+        <div className={'status'} style={{background: isOnline ? 'lawngreen' : 'crimson'}}/>
+      </div>
+      <br/><br/>
+      <div style={{opacity: 0.2}}>ID: {!errorR && !loadingR && randomId && randomId}</div>
       <h3>Влажность: {!errorH && !loadingH && humidity && humidity}</h3>
+      <h2 style={{color: getColor(temperature ?? null)}}>
+        Температура: {!errorT && !loadingT && temperature && temperature}
+      </h2>
       <div style={{display: 'grid', gridAutoFlow: 'column'}} id={'colored-line'}>
         {range.map(r =>
           <span style={{backgroundColor: r[2], textAlign: 'end'}} key={r[0]}>
